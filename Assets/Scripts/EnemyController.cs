5cb644428e2cffb920b06fc6b182b96f94e48d12
Alexey Gorbunov <alexey.gorbunov07@gmail.com>
2025-10-04 23:53:41 -0700

    feat: enemy movement ai based on wandering, fleeing, hunting states

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,39 +1,51 @@
 using UnityEngine;
 
-public enum EnemyState
-{
-    Wandering,
-    Hunting,
-    Fleeing,
-}
-
 public class EnemyController : MonoBehaviour, ICreature
 {
     [Header("Stats")]
-    [SerializeField] private int _level = 1;
-    [SerializeField] private float _health = 10f;
-    [SerializeField] private float _moveSpeed = 10f;
-    [SerializeField] private float _maxSwingSpeed = 10f;
-    [SerializeField] private float _swingAcceleration = 10f;
-    [SerializeField] private float _weight = 10f;
+    [SerializeField] private int level = 1;
+    [SerializeField] private float health = 10f;
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float maxSwingSpeed = 10f;
+    [SerializeField] private float swingAcceleration = 10f;
+    [SerializeField] private float weight = 10f;
 
-    public int Level => _level;
-    public float Health => _health;
-    public float MoveSpeed => _moveSpeed;
-    public float MaxSwingSpeed => _maxSwingSpeed;
-    public float SwingAcceleration => _swingAcceleration;
-    public float Weight => _weight;
+    [Header("Behavior")]
+    [SerializeField] private float wanderRadius = 10;
+    [SerializeField] private float huntingRadius = 10;
+    [SerializeField] private float fleeingRadius = 10;
 
-    private EnemyState state = EnemyState.Wandering;
+    public int Level => level;
+    public float Health => health;
+    public float MoveSpeed => moveSpeed;
+    public float MaxSwingSpeed => maxSwingSpeed;
+    public float SwingAcceleration => swingAcceleration;
+    public float Weight => weight;
+
+    [SerializeField] private Vector2 target;
+    [SerializeField] private Transform huntingTarget;
+    private Rigidbody2D rb; 
+    private float targetThreshold = 0.25f;
+    private LayerMask obstacles;
+    private float originalHealth;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        obstacles = LayerMask.GetMask("Default");
+        originalHealth = health;
+    }
 
     void Start()
     {
-
+        target = GetWanderingTarget();
     }
 
-    void Update()
+    void FixedUpdate()
     {
-
+        UpdateTarget();
+        Vector2 newPos = Vector2.MoveTowards(rb.position, target, moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(newPos);
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -43,11 +55,60 @@ public class EnemyController : MonoBehaviour, ICreature
         {
             float computedDamage = collision.relativeVelocity.magnitude * weapon.Damage;
             TakeDamage(computedDamage);
+            huntingTarget = collision.gameObject.GetComponent<HingeJoint2D>()?.connectedBody.gameObject.transform;
         }
     }
 
     public void TakeDamage(float computedDamage)
     {
-        _health = Mathf.Max(0, _health - computedDamage);
+        health = Mathf.Max(0, health - computedDamage);
+        if (health == 0)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject, 0.5f);
+        }
+    }
+
+    private void UpdateTarget()
+    {
+        if (huntingTarget != null && Vector2.Distance(transform.position, huntingTarget.position) > Mathf.Min(huntingRadius, fleeingRadius))
+        {
+            huntingTarget = null;
+        }
+
+        // wandering state
+        if ((huntingTarget == null || health < originalHealth / 4) && Vector2.Distance(transform.position, target) < targetThreshold)
+        {
+            // Debug.Log("wandering");
+            target = GetWanderingTarget();
+        }
+
+        // fleeing state
+        else if (huntingTarget != null && health < originalHealth / 4)
+        {
+            // Debug.Log("fleeing");
+            target = (transform.position - huntingTarget.position).normalized * fleeingRadius * 2;
+        }
+
+        // hunting state
+        else if (huntingTarget != null)
+        {
+            // Debug.Log("hunting");
+            target = huntingTarget.position;
+        }
+    }
+
+    private Vector2 GetWanderingTarget()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Vector2 wanderPoint = (Vector2)transform.position + Random.insideUnitCircle * wanderRadius;
+
+            var blocked = Physics2D.OverlapCircle(wanderPoint, 0.15f, obstacles);
+            if (blocked) continue;
+
+            return wanderPoint;
+        }
+        return transform.position;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,19 +4,19 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour, ICreature
 {
     [Header("Stats")]
-    [SerializeField] private int _level = 1;
-    [SerializeField] private float _health = 10f;
-    [SerializeField] private float _moveSpeed = 10f;
-    [SerializeField] private float _maxSwingSpeed = 10f;
-    [SerializeField] private float _swingAcceleration = 10f;
-    [SerializeField] private float _weight = 10f;
+    [SerializeField] private int level = 1;
+    [SerializeField] private float health = 10f;
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float maxSwingSpeed = 10f;
+    [SerializeField] private float swingAcceleration = 10f;
+    [SerializeField] private float weight = 10f;
 
-    public int Level => _level;
-    public float Health => _health;
-    public float MoveSpeed => _moveSpeed;
-    public float MaxSwingSpeed => _maxSwingSpeed;
-    public float SwingAcceleration => _swingAcceleration;
-    public float Weight => _weight;
+    public int Level => level;
+    public float Health => health;
+    public float MoveSpeed => moveSpeed;
+    public float MaxSwingSpeed => maxSwingSpeed;
+    public float SwingAcceleration => swingAcceleration;
+    public float Weight => weight;
 
     private InputSystem_Actions controls;
     private Vector2 moveInput;
@@ -49,7 +49,7 @@ public class PlayerController : MonoBehaviour, ICreature
 
     void Update()
     {
-        rb.linearVelocity = _moveSpeed * moveInput;
+        rb.linearVelocity = moveSpeed * moveInput;
     }
 
     private void OnMovePerformed(InputAction.CallbackContext ctx)
@@ -74,6 +74,6 @@ public class PlayerController : MonoBehaviour, ICreature
 
     public void TakeDamage(float incomingDamage)
     {
-        _health = Mathf.Max(0, _health - incomingDamage);
+        health = Mathf.Max(0, health - incomingDamage);
     }
 }
